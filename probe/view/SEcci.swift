1688190022982a99e4642353e90a17976fca21d7
Fray Pineda <fraypineda@imac-de-fray.local>
2019-01-18 19:41:19 -0400

    seca

---
+++ probe/view/SEcci.swift
@@ -0,0 +1,35 @@
+//
+//  SEcci.swift
+//  probe
+//
+//  Created by Fray Pineda on 1/18/19.
+//  Copyright © 2019 david. All rights reserved.
+//
+
+import UIKit
+
+class SEcci: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
